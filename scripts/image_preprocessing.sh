da72e5c2fda94155e29522dc1b3453a2d7f862f3
Maximilian Körner <maximilian.koerner@gmx.com>
2017-12-17 15:43:49 +0100

    scripts

--- scripts/image_preprocessing.sh
+++ scripts/image_preprocessing.sh
@@ -1,6 +1,7 @@
+#check if data folder already exists and prompt if it should be reset
 if [ -d "../data" ]; then
     # Control will enter here if $DIRECTORY exists.
-    read -p "Data folder already existing!\nDELETE DATA FOLDER AND RECREATE DATA? y/N " -n 1 -r
+    read -p "Warning: Data folder already existing! DELETE DATA FOLDER AND RECREATE DATA? y/N " -n 1 -r
     echo    # (optional) move to a new line
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
@@ -10,6 +11,17 @@ if [ -d "../data" ]; then
         exit 1
     fi
 fi
+#create data folder and copy images from NAS to local folder
 mkdir ../data
+mkdir ../data/raw
 echo "'data' folder created"
-echo "copying image data from NAS to 'data'"
+
+#check if NAS is found
+if [ ! -d "/media/nas/01_Datasets/CT/LITS/Training Batch 1" ]; then
+    echo "Error: data source not found! Is the NAS mounted correctly to '/media/nas'?"
+    exit 1
+fi
+
+echo "copying image data from NAS to 'data/raw'"
+rsync --progress /media/nas/01_Datasets/CT/LITS/Training Batch 1 ../data/raw
+rsync --progress /media/nas/01_Datasets/CT/LITS/Training Batch 2 ../data/raw
